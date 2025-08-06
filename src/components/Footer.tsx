85dcd6bb71299778a78f64593f89a87fc25a526f
sn <scarsan62@gmail.com>
2025-08-06 14:11:08 +0300

    finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,32 @@
+import { Mail, Phone } from "lucide-react";
 import { PAGES_HEIGHTS } from "../lib/constants";
 
 const Footer = () => {
   return (
     <footer
       style={{ height: PAGES_HEIGHTS.FOOTER }}
-      className="bg-bg-dark border-border border-b-4"
+      className="from-bg-dark to-bg bg-gradient-to-t from-90%"
     >
-      <p className="text-center text-2xl">footer</p>
+      <div className="mx-auto w-fit pt-26">
+        <p className="text-xl">Quick Contact</p>
+        <ul className="mt-2 space-y-2 text-sm">
+          <li>
+            <a href="mailto:stefannita62@yahoo.com" className="hover:underline">
+              <Mail size={16} className="mr-1 inline" /> stefannita62@yahoo.com
+            </a>
+          </li>
+          <li>
+            <a href="tel:+40784311008" className="hover:underline">
+              <Phone size={16} className="mr-1 inline" /> +40 784 311 008
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="text-text-muted mx-auto w-fit pt-26 text-sm">
+        © {new Date().getFullYear()} Ștefan Niță. All rights reserved.
+      </div>
+      <div className="bg-text-muted mt-2 h-px"></div>
     </footer>
   );
 };
